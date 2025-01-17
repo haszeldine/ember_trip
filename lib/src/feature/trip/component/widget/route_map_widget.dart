3ad2684596330bccb1f780cea5f52dd47df7dc15
Calum Thain <calum.thain@gmail.com>
2025-01-17 10:48:02 +0000

    Simplify build method, lift null checks higher

--- lib/src/feature/trip/component/widget/route_map_widget.dart
+++ lib/src/feature/trip/component/widget/route_map_widget.dart
@@ -26,34 +26,39 @@ class _RouteMapWidgetState extends State<RouteMapWidget> {
       return ErrorWidget.withDetails(message: 'Failed to load route data');
     }
 
-    onMapCreated(mapboxMap) {
-      mapboxMap.setCamera(
+    if (_annotationManager != null) {
+      _updateAnnotations(_annotationManager!, routeNodes);
+    }
+
+    return MapWidget(
+      key: ValueKey('mapWidget'),
+      onMapCreated: _onMapCreatedPartial(routeNodes),
+    );
+  }
+
+  Future<void> Function(MapboxMap) _onMapCreatedPartial(
+      final List<RouteNodeData> routeNodes) {
+    return (MapboxMap map) async {
+      map.setCamera(
         CameraOptions(
           center: Point(coordinates: Position(-3.20, 56.77)),
           zoom: 7,
         ),
       );
-      mapboxMap.annotations
-          .createCircleAnnotationManager()
-          // This looks pretty hacky, but it's from the SDK's own examples
-          .then((manager) => _annotationManager = manager)
-          .then((manager) => _updateAnnotations(manager, routeNodes));
-    }
-
-    _updateAnnotations(_annotationManager, routeNodes);
-    return MapWidget(key: ValueKey('mapWidget'), onMapCreated: onMapCreated);
+      _annotationManager =
+          await map.annotations.createCircleAnnotationManager();
+      _updateAnnotations(_annotationManager!, routeNodes);
+    };
   }
 
-  void _updateAnnotations(annotationManager, List<RouteNodeData> routeNodes) {
-    annotationManager?.deleteAll();
-    annotationManager?.createMulti(
-      List<CircleAnnotationOptions>.unmodifiable(
-        routeNodes.where(_isValidGeoData).map(_buildNodeAnnotation),
-      ),
-    );
+  void _updateAnnotations(
+      final CircleAnnotationManager manager, final List<RouteNodeData> routeNodes) {
+    manager.deleteAll();
+    manager.createMulti(
+        routeNodes.where(_isValidGeoData).map(_buildNodeAnnotation).toList());
   }
 
-  bool _isValidGeoData(RouteNodeData node) {
+  bool _isValidGeoData(final RouteNodeData node) {
     return node.location.latitude != null && node.location.longitude != null;
   }
 
